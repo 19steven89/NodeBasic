8c039190f70962222fb7ab097bc42c17ea9815ce
Steven Barry SE2015 <steven.barry.2016@uni.strath.ac.uk>
2018-04-03 21:45:10 +0100

    utility file used as an example of how to debug in cli, which can be done by running: node inspect app.js read --title="note title" in the cli to read the note and press c to run up to the point where the debugger statement is and then use repl to inspect the note array by inputting note after repl

---
+++ playground/debugging.js
@@ -0,0 +1,26 @@
+var person = {
+  name: "steven"
+};
+
+person.age = "25";
+//used to debug app in cli
+//using node inspect <filename>
+//then in debug mode use n for next
+//and c for continue
+//the debugger will stop the app in debug mode at this line when using c
+//for continue to check the app
+debugger;
+person.name = "Mike";
+console.log(person);
+
+//using repl in debug mode can check the value of certain properties like
+//person.age which will say 25 if app has reached line 5
+
+
+//to run with debugger in CLi use
+//node inspect app.js read --title="note title"
+//this starts the app with the read command and then c to reach the "debugger;"
+//statement
+//use repl command followed by note to print the array note value passed in
+
+//***very useful debuggin tool***
